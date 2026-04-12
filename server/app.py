2630d829a54b97508d80153328aebac55f2e733e
Anubhav Gupta <anubhavgupta@Anubhavs-MacBook-Pro.local>
2026-04-12 15:25:19 +0530

    fix/chore: update server launcher to use sys.executable - Guarantees compatibility on different Docker containers by pointing directly to the active python interpreter.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 import subprocess
 import os
 
+import sys
+
 def main():
     root_app = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "app.py")
-    subprocess.run(["python", root_app])
+    subprocess.run([sys.executable, root_app])
 
 if __name__ == "__main__":
     main()
